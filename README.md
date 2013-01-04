4b4d51ce14147c86877b65cb58931081f27ec426
Daniel Davidson <dan.davidson.uk@gmail.com>
2013-01-04 02:41:43 +0000

    updated readme, added main plugin file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-blanq-bootstrap-menu
+Blanq Bootstrap Menu
 ====================
 
-This is a plug-in for Blanq WordPress theme which extends Walker_Nav_Menu to integrate Bootstrap 
+This is a plug-in for Blanq WordPress theme which extends Walker_Nav_Menu to integrate Bootstrap. The <a href="https://gist.github.com/1597994">original extended class</a> was written by <a href="https://gist.github.com/johnmegahan">johnmegahan</a>.
---
+++ blanq-bootstrap-menu.php
@@ -0,0 +1,12 @@
+<?php
+   /*
+   Plugin Name: Blanq Bootstrap Menu
+   Plugin URI: https://github.com/da-n/blanq-bootstrap-menu
+   Description: This is a plug-in for Blanq WordPress theme which extends Walker_Nav_Menu to integrate Bootstrap. The <a href="https://gist.github.com/1597994">original extended class</a> was written by <a href="https://gist.github.com/johnmegahan">johnmegahan</a>.
+   Version: 1.0
+   Author: Daniel Davidson
+   Author URI: http://github.com/da-n
+   License: GPL2
+   */
+?>
+
